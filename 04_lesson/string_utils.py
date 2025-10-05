0f2ecd358e100f5c2d866d82bb38a823744c55a2
ольга <jkmuf1981>
2025-10-05 09:16:06 +0300

    lesson4

--- 04_lesson/string_utils.py
+++ 04_lesson/string_utils.py
@@ -1,23 +1,54 @@
+
 class StringUtils:
-    @staticmethod
-    def is_palindrome(s):
-        """Проверяет строку на палиндром"""
-        return s.lower() == s.lower()[::-1]
+    """
+    Класс с полезными утилитами для обработки и анализа строк
+    """
 
-    @staticmethod
-    def count_words(text):
-        """Подсчет количества слов в тексте."""
-        words = text.split()
-        return len(words)
+    def capitalize(self, string: str) -> str:
+        """
+        Принимает на вход текст, делает первую букву заглавной
+        и возвращает этот же текст
+        Пример: `capitalize("skypro") -> "Skypro"`
+        """
+        return string.capitalize()
 
-    @staticmethod
-    def reverse_string(s):
-        """Разворот строки."""
-        return s[::-1]
+    def trim(self, string: str) -> str:
+        """
+        Принимает на вход текст и удаляет пробелы в начале, если они есть
+        Пример: `trim("   skypro") -> "skypro"`
+        """
+        whitespace = " "
+        while string.startswith(whitespace):
+            string = string.removeprefix(whitespace)
+        return string
 
-    @staticmethod
-    def capitalize_first_letter(s):
-        """Делает первую букву заглавной."""
-        if not s.strip():
-            raise ValueError("Строка пустая")
-        return s[:1].upper() + s[1:]
+    def contains(self, string: str, symbol: str) -> bool:
+        """
+        Возвращает `True`, если строка содержит искомый символ
+        и `False` - если нет
+        Параметры:
+            `string` - строка для обработки
+            `symbol` - искомый символ
+        Пример 1: `contains("SkyPro", "S") -> True`
+        Пример 2: `contains("SkyPro", "U") -> False`
+        """
+        res = False
+        try:
+            res = string.index(symbol) > -1
+        except ValueError:
+            pass
+
+        return res
+
+    def delete_symbol(self, string: str, symbol: str) -> str:
+        """
+        Удаляет все подстроки из переданной строки
+        Параметры:
+            `string` - строка для обработки
+            `symbol` - искомый символ для удаления
+        Пример 1: `delete_symbol("SkyPro", "k") -> "SyPro"`
+        Пример 2: `delete_symbol("SkyPro", "Pro") -> "Sky"`
+        """
+        if self.contains(string, symbol):
+            string = string.replace(symbol, "")
+        return string
